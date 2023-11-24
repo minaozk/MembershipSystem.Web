1715928d817887d343215886972a30906f9c154c
minaozk <80757513+minaozk@users.noreply.github.com>
2023-11-24 15:29:08 +0300

    Add project files.

---
+++ MembershipSystem.Web/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MembershipSystem.Web.Models
+{
+	public class AppDbContext : IdentityDbContext<AppUser,AppRole,string>
+	{
+		public AppDbContext(DbContextOptions options):base(options)
+		{
+			
+		}
+
+	}
+}
---
+++ MembershipSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MembershipSystem.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
